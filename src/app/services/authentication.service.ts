0875ab84f2d2e959090d8f9f617e750bd8b22611
Joseeph5 <youssef.beenamor@gmail.com>
2019-04-23 16:33:30 +0100

    update

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -13,6 +13,9 @@ export class AuthenticationService {
 
   
   AuthRL='http://fleet.tn/ws_rimtrack_all/signin';
+  PathsURL='http://fleet.tn/ws_rimtrack_all/paths/813785';
+
+
   handleError(error) {
     let errorMessage = '';
     if(error.error instanceof ErrorEvent) {
@@ -27,7 +30,8 @@ export class AuthenticationService {
   }
   httpOptions = {
    headers: new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Authorization':'eyJhbGciOiJIUzUxMiJ9.eyJzdWJpZCI6bnVsbCwic3ViIjoiZWNvdGkiLCJhdWRpZW5jZSI6IndlYiIsImNyZWF0ZWQiOjE1NTU5NjkxMjc5MDksImlzcm9vdCI6dHJ1ZSwiaWQiOjEyNDIsImV4cCI6MTU1NjU3MzkyN30.qsFr25nD36UVJEjStHB586d2XV8bJrrM0ICOhpbs-opzrPoNyc8R0gyDzQTivfNgxDls6_X7s5y0PaOT0NrTPw'
    })
   }  
   constructor(private http: HttpClient) {
@@ -37,6 +41,16 @@ export class AuthenticationService {
 
   login(username: string, password: string) {
     return this.http.post<any>(this.AuthRL, { username, password })
+    .pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  GetPaths(startDate: string, endDate: string) {
+    
+    
+    return this.http.post<any>(this.PathsURL, { startDate, endDate },this.httpOptions)
     .pipe(
       retry(1),
       catchError(this.handleError)
